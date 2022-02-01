8e8e9978b6f630d33ab5233387a7834663da17bd
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2022-02-01 07:22:00 -1000

    Better zoom config

--- CircuitPython 7.x/macros/mac-zoom.py
+++ CircuitPython 7.x/macros/mac-zoom.py
@@ -1,7 +1,8 @@
 # MACROPAD Hotkeys: Tower application for Mac
 # Contributed by Me
 
-from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 app = {                      # REQUIRED dict, must be named 'app'
     'name' : 'zoom.us', # Application name
@@ -12,9 +13,9 @@ app = {                      # REQUIRED dict, must be named 'app'
         (0x004000, 'Video', [Keycode.SHIFT, Keycode.COMMAND, 'v']),
         (0x000000, '', []),
         # 2nd row ----------
+        (0x000020, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),
         (0x000000, '', []),
-        (0x000000, '', []),
-        (0x000000, '', []),
+        (0x000020, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
         # 3rd row ----------
         (0x000000, '', []),
         (0x000000, '', []),
@@ -22,7 +23,7 @@ app = {                      # REQUIRED dict, must be named 'app'
         # 4th row ----------
         (0x000000, '', []),
         (0x000000, '', []),
-        (0x000000, '', []),
+        (0xff0000, 'Leave', [Keycode.COMMAND, 'w', -Keycode.COMMAND, '\n']),
         # Encoder button ---
         (0x000000, '', [])
     ]
